fc5c931cbca2031632e2400fd0cfb41bc4f34372
Nirojan21 <nirojan.yoga@gmail.com>
2022-10-17 13:11:55 +0530

    add OTP screen

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import {SafeAreaView} from 'react-native';
 import Gradient from './Components/GaradientBG/Gradient';
 import Signup from './Screens/Customer/Signup/Signup';
 import Login from './Screens/Login/Login';
+import OTP from './Screens/OtpValidation/OTP';
 import RegisterAs from './Screens/Register/RegisterAs';
 
 const App = () => {
@@ -11,7 +12,8 @@ const App = () => {
       <Gradient>
         {/* <Login /> */}
         {/* <RegisterAs /> */}
-        <Signup/>
+        {/* <Signup/> */}
+        <OTP/>
       </Gradient>
     </SafeAreaView>
   );
