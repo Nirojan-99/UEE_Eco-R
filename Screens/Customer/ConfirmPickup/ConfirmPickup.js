924cae6a302363874c96dbb392c9a51746e0378e
Nirojan21 <nirojan.yoga@gmail.com>
2022-10-21 22:01:32 +0530

    add confirmation screen

--- Screens/Customer/ConfirmPickup/ConfirmPickup.js
+++ Screens/Customer/ConfirmPickup/ConfirmPickup.js
@@ -1,16 +1,43 @@
-import {View, Text} from 'react-native';
+import {View, Text, Image, SafeAreaView, Dimensions} from 'react-native';
 import React from 'react';
 import Title from '../../../Components/Title/Title';
-import ShadowGradientBG from '../../../Components/ShadowGradiendBG/ShadowGradientBG';
+import tick from '../../../Assets/tick.png';
+import anime from '../../../Assets/confirm.png';
 
 export default function ConfirmPickup() {
-  return (
-    <View className="p-3">
-      <Title text={'Confirmation'} />
 
-      <ShadowGradientBG>
-        <Text>fdd</Text>
-      </ShadowGradientBG>
-    </View>
+  const width = Dimensions.get('screen').width;
+  
+  return (
+    <SafeAreaView className="my-3">
+      <View className="mx-3">
+        <Title text={'Confirmation'} />
+      </View>
+      <View className="mt-5 bg-blue-100 p-3 rounded-xl mx-3">
+        <View className="flex-row items-center justify-start ">
+          <Image source={tick} className="h-8 w-8" />
+          <Text
+            className="text-black font-semibold text-[15px] ml-2 flex-1"
+            numberOfLines={1}>
+            Pickup Request is successfully sent
+          </Text>
+        </View>
+        <View className="my-2">
+          <Text className="text-[#1C6758] text-md font-semibold">
+            Waiting for a dealer to accept the pickup...
+          </Text>
+        </View>
+        <View className="my-2">
+          <Text
+            className="text-gray-400 text-md font-semibold"
+            numberOfLines={2}>
+            Keep you products ready to be collected by the dealers.
+          </Text>
+        </View>
+      </View>
+      <View className=" flex-1 w-full mt-20">
+        <Image className="h-80" style={{width: width}} source={anime} />
+      </View>
+    </SafeAreaView>
   );
 }
