a3395191215f615604c11a2598374c539a903df0
Nirojan21 <nirojan.yoga@gmail.com>
2022-10-22 11:14:17 +0530

    add security tab screen

--- Screens/Customer/Security/Security.js
+++ Screens/Customer/Security/Security.js
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, KeyboardAvoidingView, ScrollView} from 'react-native';
+import React from 'react';
+import Label from '../../../Components/Label/Label';
+import Input from '../../../Components/Input/Input';
+import ContainedButton from '../../../Components/Button/ContainedButton';
 
 export default function Security() {
   return (
-    <View>
-      <Text>Security</Text>
-    </View>
-  )
+    <KeyboardAvoidingView behavior="height" className="mb-6">
+      <ScrollView>
+        <View className="mx-3 mt-5">
+          <Label text={'Old Password'} />
+          <Input type={'password'} />
+        </View>
+        <View className="mx-3 mt-1">
+          <Label text={'New Password'} />
+          <Input type={'password'} />
+        </View>
+        <View className="mx-3 mt-1">
+          <Label text={'Confirm Password'} />
+          <Input type={'password'} />
+        </View>
+        <View className="mx-3 mt-7">
+          <ContainedButton text={'UPDATE'} submitHandler={() => {}} />
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
 }
