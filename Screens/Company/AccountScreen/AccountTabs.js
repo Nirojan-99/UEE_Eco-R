00af6cefd9d7519c10099dc5c8e6c40c997e8da2
Nirojan21 <nirojan.yoga@gmail.com>
2022-11-04 16:38:42 +0530

    add payments screen

--- Screens/Company/AccountScreen/AccountTabs.js
+++ Screens/Company/AccountScreen/AccountTabs.js
@@ -11,7 +11,7 @@ export default function AccountTabs() {
   const Tab = createMaterialTopTabNavigator();
   return (
     <Tab.Navigator
-      initialRouteName="Profile"
+      initialRouteName="Payment"
       tabBar={props => (
         <View>
           <CustomTabBar {...props} />
--- Screens/Company/Transaction/Transaction.js
+++ Screens/Company/Transaction/Transaction.js
@@ -1,10 +1,11 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text} from 'react-native';
+import React from 'react';
+import {SafeAreaView} from 'react-native';
 
 export default function Transaction() {
   return (
-    <View>
-      <Text>Transaction</Text>
-    </View>
-  )
+    <SafeAreaView>
+      <View></View>
+    </SafeAreaView>
+  );
 }
