dcc06601aceac9abbd7e36fd3eb74329b51c3110
Sayanthan-K <kathirsayanthan@gmail.com>
2022-11-08 23:19:25 +0530

    add some data in store

--- Store/Company/InsightsData.js
+++ Store/Company/InsightsData.js
@@ -1,33 +1,33 @@
 insightsData = [
   {
-    productsname: 'used plastic waste',
-    totalcollectionofkg: '12',
-    totalcollectionofamount: '1200',
+    productName: 'used plastic waste',
+    totalCollectionofKg: '12',
+    totalCollectionOfAmount: '1200',
   },
   {
-    productsname: 'used Paper waste',
-    totalcollectionofkg: '6',
-    totalcollectionofamount: '1000',
+    productName: 'used Paper waste',
+    totalCollectionofKg: '6',
+    totalCollectionOfAmount: '1000',
   },
   {
-    productsname: 'used food waste',
-    totalcollectionofkg: '15',
-    totalcollectionofamount: '1500',
+    productName: 'used food waste',
+    totalCollectionofKg: '15',
+    totalCollectionOfAmount: '1500',
   },
   {
-    productname: 'used faculty waste',
-    totalcollectionofkg: '5',
-    totalcollectionofamount: '800',
+    productName: 'used faculty waste',
+    totalCollectionofKg: '5',
+    totalCollectionOfAmount: '800',
   },
   {
-    productname: 'used Agricultural waste',
-    totalcollectionofkg: '12',
-    totalcollectionofamount: '1800',
+    productName: 'used Agricultural waste',
+    totalCollectionofKg: '12',
+    totalCollectionOfAmount: '1800',
   },
   {
-    productname: 'used plastic waste',
-    totalcollectionofkg: '18',
-    totalcollectionofamount: '1300',
+    productName: 'used plastic waste',
+    totalCollectionofKg: '18',
+    totalCollectionOfAmount: '1300',
   },
 ];
 export {insightsData};
--- Store/Customer/TransactionCustomer.js
+++ Store/Customer/TransactionCustomer.js
@@ -1,68 +1,93 @@
 const TransactionCustomer = [
   {
-    companyname: 'Biffa group limited',
+    companyName: 'Biffa group limited',
     date: '06/11/2022',
     prices: '100',
   },
   {
-    companyname: 'Hitachi Corporation',
+    companyName: 'Hitachi Corporation',
     date: '05/11/2022',
     prices: '70',
   },
   {
-    companyname: 'Suez limited',
+    companyName: 'Suez limited',
     date: '04/11/2022',
     prices: '60',
   },
   {
-    companyname: 'Covanta Corporation',
+    companyName: 'Covanta Corporation',
     date: '03/11/2022',
     prices: '110',
   },
   {
-    companyname: 'Suez limited',
+    companyName: 'Suez limited',
     date: '02/11/2022',
     prices: '95',
   },
   {
-    companyname: 'Veolia limited',
+    companyName: 'Veolia limited',
     date: '01/11/2022',
     prices: '80',
   },
   {
-    companyname: 'Suez limited',
+    companyName: 'Covanta Corporation',
     date: '04/11/2022',
     prices: '85',
   },
   {
-    companyname: 'Biffa group limited',
+    companyName: 'Biffa group limited',
     date: '24/10/2022',
     prices: '94',
   },
   {
-    companyname: 'Veolia limited',
+    companyName: 'Veolia limited',
     date: '22/10/2022',
     prices: '150',
   },
   {
-    companyname: 'Suez limited',
+    companyName: 'Suez limited',
     date: '21/10/2022',
     prices: '120',
   },
   {
-    companyname: 'Biffa group limited',
+    companyName: 'Biffa group limited',
     date: '20/10/2022',
     prices: '130',
   },
   {
-    companyname: 'Biffa group limited',
+    companyName: 'Veolia limited',
     date: '26/10/2022',
     prices: '140',
   },
   {
-    companyname: 'Biffa group limited',
-    date: '25/10/2022',
+    companyName: 'Biffa group limited',
+    date: '24/10/2022',
     prices: '145',
+  },
+  {
+    companyName: 'Suez limited',
+    date: '22/10/2022',
+    prices: '135',
+  },
+  {
+    companyName: 'Biffa group limited',
+    date: '12/10/2022',
+    prices: '205',
+  },
+  {
+    companyName: 'Hitachi Corporation',
+    date: '15/10/2022',
+    prices: '175',
+  },
+  {
+    companyName: 'Biffa group limited',
+    date: '12/10/2022',
+    prices: '165',
+  },
+  {
+    companyName: 'Suez limited',
+    date: '02/10/2022',
+    prices: '245',
   },
 ];
 export {TransactionCustomer};
